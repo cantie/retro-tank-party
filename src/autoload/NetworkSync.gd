9a8fe6b63d07f01cf01f2a2a611416809539df5c
David Snopek <dsnopek@gmail.com>
2021-07-13 21:11:58 -0500

    Lower bytes count for debug message now that messages are so much smaller

--- src/autoload/NetworkSync.gd
+++ src/autoload/NetworkSync.gd
@@ -91,7 +91,7 @@ func _ready() -> void:
 	
 	# Tweak some settings
 	#SyncManager.max_buffer_size = 20
-	SyncManager.debug_message_bytes = 220
+	SyncManager.debug_message_bytes = 150
 	#SyncManager.max_input_frames_per_message = 5
 	#SyncManager.max_messages_at_once = 2
 	SyncManager.interpolation = true
