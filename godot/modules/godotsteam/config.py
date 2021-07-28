a3c0dc0e50e8195242db16e8ce8774e1e5e2cf2e
David Snopek <dsnopek@gmail.com>
2021-07-28 10:06:19 -0500

    Try this?

--- godot/modules/godotsteam/config.py
+++ godot/modules/godotsteam/config.py
@@ -2,16 +2,17 @@ def can_build(env, platform):
 	return platform=="x11" or platform=="windows" or platform=="osx" or platform=="server"
 
 def configure(env):
-	env.Append(CPPPATH=["sdk/public/"])
+	module_path = Dir('.').srcnode().abspath
+	env.Append(CPPPATH=["%s/sdk/public/" % module_path])
 
 	# If compiling Linux
 	if env["platform"]== "x11" or env["platform"] == "server":
 		env.Append(LIBS=["steam_api"])
 		env.Append(RPATH=["."])
 		if env["bits"]=="32":
-			env.Append(LIBPATH=["sdk/redistributable_bin/linux32"])
+			env.Append(LIBPATH=["%s/sdk/redistributable_bin/linux32" % module_path])
 		else: # 64 bit
-			env.Append(LIBPATH=["sdk/redistributable_bin/linux64"])
+			env.Append(LIBPATH=["%s/sdk/redistributable_bin/linux64" % module_path])
 
 	# If compiling Windows
 	elif env["platform"] == "windows":
@@ -19,24 +20,24 @@ def configure(env):
 		if env["CC"] == "cl":
 			if env["bits"]=="32":
 				env.Append(LINKFLAGS=["steam_api.lib"])
-				env.Append(LIBPATH=["sdk/redistributable_bin"])
+				env.Append(LIBPATH=["%s/sdk/redistributable_bin" % module_path])
 			else: # 64 bit
 				env.Append(LINKFLAGS=["steam_api64.lib"])
-				env.Append(LIBPATH=["sdk/redistributable_bin/win64"])
+				env.Append(LIBPATH=["%s/sdk/redistributable_bin/win64" % module_path])
 		
 		# Mostly "GCC"
 		else:
 			if env["bits"]=="32":
 				env.Append(LIBS=["steam_api"])
-				env.Append(LIBPATH=["sdk/redistributable_bin"])
+				env.Append(LIBPATH=["%s/sdk/redistributable_bin" % module_path])
 			else: # 64 bit
 				env.Append(LIBS=["steam_api64"])
-				env.Append(LIBPATH=["sdk/redistributable_bin/win64"])
+				env.Append(LIBPATH=["%s/sdk/redistributable_bin/win64" % module_path])
 
 	# If compiling OSX
 	elif env["platform"] == "osx":
 		env.Append(LIBS=["steam_api"])
-		env.Append(LIBPATH=['sdk/redistributable_bin/osx'])
+		env.Append(LIBPATH=['%s/sdk/redistributable_bin/osx' % module_path])
 
 def get_doc_classes():
 	return [
