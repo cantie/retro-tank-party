65f1a5c4ee718a7f7764de434748f3ced2c863b6
David Snopek <dsnopek@gmail.com>
2021-08-19 17:41:30 -0500

    Pack everything for PhysicsTest.tscn into PhysicsTest.gd

--- PhysicsTest.gd
+++ PhysicsTest.gd
@@ -1,26 +1,24 @@
 extends Node2D
 
-func _ready() -> void:
-	#transform *= $Character.transform.affine_inverse()
-	
-	pass
-	#var rect1 = SGPhysics2DServer.create_rectangle_shape(0, 0, 10, 10)
-	#var rect2 = SGPhysics2DServer.create_rectangle_shape(20, 20, 5, 5)
-	#var rect3 = SGPhysics2DServer.create_rectangle_shape(5, 5, 10, 10)
-	
-	#print ("rect1 collides with rect2: %s" % SGPhysics2DServer.shape_overlaps(rect1, rect2))
-	#print ("rect1 collides with rect3: %s" % SGPhysics2DServer.shape_overlaps(rect1, rect3))
-	
-	#get_tree().quit()
-	#update()
+onready var character = $Character
 
-#func _draw() -> void:
-#	var rect1_extents = $Character/SGCollisionShape2D.shape.get_extents().to_float()
-#	draw_rect(Rect2(Vector2.ZERO, rect1_extents), Color(1.0, 1.0, 1.0, 1.0), true)
-#
-#	var rect2_extents = $Area/SGCollisionShape2D.shape.get_extents().to_float()
-#	var rect2 = Rect2($Area/SGCollisionShape2D.global_position - rect2_extents, rect2_extents * 2.0)
-#	#var transform2 = $Area/SGCollisionShape2D.global_transform * $Character/SGCollisionShape2D.global_transform.affine_inverse()
-#	var transform2 = $Character/SGCollisionShape2D.global_transform.affine_inverse() * $Area/SGCollisionShape2D.global_transform
-#	rect2 = transform2.xform(rect2)
-#	draw_rect(rect2, Color(1.0, 1.0, 1.0, 1.0), true)
+var vector := SGFixed.vector2(65536, 0)
+var counter := 0
+
+func _physics_process(delta: float) -> void:
+	# Move 5 pixels per frame.
+	character.fixed_position.iadd(vector.mulf(5*65536))
+	
+	#print ("%s, %s" % [fixed_position.x, fixed_position.y])
+	character.sync_to_physics_engine()
+	
+	counter += 1
+	if counter > 50:
+		vector.x = -vector.x
+		counter = 0
+	
+	var areas = character.get_overlapping_areas()
+	if areas.size() > 0:
+		character.modulate = Color(1.0, 0.0, 0.0)
+	else:
+		character.modulate = Color(1.0, 1.0, 1.0)
