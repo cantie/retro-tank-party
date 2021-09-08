1e1fcf61427923476323174a2d1de2a930472ee5
David Snopek <dsnopek@gmail.com>
2021-09-08 17:11:08 -0500

    Started converting map start positions to fixed-point

--- godot/modules/sg_physics_2d/register_types.cpp
+++ godot/modules/sg_physics_2d/register_types.cpp
@@ -28,6 +28,7 @@
 
 #include "./math/sg_fixed_singleton.h"
 #include "./math/sg_fixed_vector2.h"
+#include "./scene/2d/sg_fixed_position_2d.h"
 #include "./scene/2d/sg_area_2d.h"
 #include "./scene/2d/sg_static_body_2d.h"
 #include "./scene/2d/sg_kinematic_body_2d.h"
@@ -45,6 +46,8 @@ void register_sg_physics_2d_types() {
     ClassDB::register_class<SGFixed>();
     ClassDB::register_class<SGFixedVector2>();
 
+    ClassDB::register_class<SGFixedNode2D>();
+    ClassDB::register_class<SGFixedPosition2D>();
     ClassDB::register_virtual_class<SGCollisionObject2D>();
     ClassDB::register_class<SGArea2D>();
     ClassDB::register_class<SGStaticBody2D>();
