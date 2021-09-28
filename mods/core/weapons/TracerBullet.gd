681288c5f8d7f0beba1205451fe7e91d67d4072c
David Snopek <dsnopek@gmail.com>
2021-09-28 10:45:13 -0500

    Started porting the Tracer bullets over the new physics engine (incomplete)

--- mods/core/weapons/TracerBullet.gd
+++ mods/core/weapons/TracerBullet.gd
@@ -1,12 +1,13 @@
 extends "res://src/objects/Bullet.gd"
 
-var target_seek_speed := 10
+var target_seek_speed := 21845
 
 var target: Node2D = null
 
-func _process(delta: float) -> void:
+func _network_process(delta: float, input: Dictionary) -> void:
+	._network_process(delta, input)
 	if target and is_instance_valid(target):
-		var target_vector = (target.global_position - global_position).normalized()
-		vector = vector.linear_interpolate(target_vector, target_seek_speed * delta).normalized()
-		rotation = vector.angle()
+		var target_vector = target.get_global_fixed_position().sub(get_global_fixed_position()).normalized()
+		vector = vector.linear_interpolate(target_vector, target_seek_speed).normalized()
+		fixed_rotation = vector.angle()
 
