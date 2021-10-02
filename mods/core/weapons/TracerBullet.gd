83244b1fadec03727af2e1b9696fc9c733cea15c
David Snopek <dsnopek@gmail.com>
2021-10-02 10:37:17 -0500

    Fix issue with tracer losing target during rollback

--- mods/core/weapons/TracerBullet.gd
+++ mods/core/weapons/TracerBullet.gd
@@ -4,6 +4,24 @@ var target_seek_speed := 21845
 
 var target: Node2D = null
 
+func _network_spawn_preprocess(info: Dictionary) -> Dictionary:
+	var res := ._network_spawn_preprocess(info)
+	res['target'] = info['target'].get_path() if info['target'] != null else null
+	return res
+
+func _network_spawn(info: Dictionary) -> void:
+	._network_spawn(info)
+	target = get_node(info['target']) if info['target'] != null else null
+
+func _save_state() -> Dictionary:
+	var state = ._save_state()
+	state['target'] = target.get_path() if target else null
+	return state
+
+func _load_state(state: Dictionary) -> void:
+	._load_state(state)
+	target = get_node(state['target']) if state['target'] != null else null
+
 func _network_process(delta: float, input: Dictionary) -> void:
 	._network_process(delta, input)
 	if target and is_instance_valid(target):
--- src/components/weapons/BaseWeapon.gd
+++ src/components/weapons/BaseWeapon.gd
@@ -17,11 +17,10 @@ func attach_weapon() -> void:
 func detach_weapon() -> void:
 	pass
 
-func create_bullet():
-	return SyncManager.spawn("Bullet", tank.get_parent(), weapon_type.bullet_scene, {
-		tank = tank,
-		weapon_type = weapon_type,
-	})
+func create_bullet(info: Dictionary = {}):
+	info['tank'] = tank
+	info['weapon_type'] = weapon_type
+	return SyncManager.spawn("Bullet", tank.get_parent(), weapon_type.bullet_scene, info)
 
 func fire_weapon() -> void:
 	create_bullet()
