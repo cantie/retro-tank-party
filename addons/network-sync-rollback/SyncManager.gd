3d923826917f0b5dc72bb4987855037dea7cd68b
David Snopek <dsnopek@gmail.com>
2021-10-26 17:23:10 -0500

    PerfTimer.get_total() so we can only show numbers when it exceeds some value

--- addons/network-sync-rollback/SyncManager.gd
+++ addons/network-sync-rollback/SyncManager.gd
@@ -503,14 +503,23 @@ func _call_predict_remote_input(previous_input: Dictionary, ticks_since_real_inp
 	return input
 
 func _call_network_process(delta: float, input_frame: InputBufferFrame) -> void:
+	#var perf = PerfTimer.new()
+	
 	var nodes: Array = get_tree().get_nodes_in_group('network_sync')
 	var i = nodes.size()
 	while i > 0:
 		i -= 1
 		var node = nodes[i]
 		if node.has_method('_network_process') and node.is_inside_tree() and not node.is_queued_for_deletion():
+			#var node_path = str(node.get_path())
+			#perf.start("_network_process: %s" % node_path)
 			var player_input = input_frame.get_player_input(node.get_network_master())
 			node._network_process(delta, player_input.get(str(node.get_path()), {}))
+			#perf.stop("_network_process: %s" % node_path)
+	
+	#if perf.get_total() >= 1000:
+	#	print (" ---")
+	#	perf.print_timings()
 
 func _call_save_state() -> Dictionary:
 	#var perf = PerfTimer.new()
@@ -527,8 +536,9 @@ func _call_save_state() -> Dictionary:
 				state[node_path] = node._save_state()
 				#perf.stop("_save_state: %s" % node_path)
 	
-	#print (" ---")
-	#perf.print_timings()
+	#if perf.get_total() >= 1000:
+	#	print (" ---")
+	#	perf.print_timings()
 	
 	return state
 
--- addons/network-sync-rollback/debugger/PerfTimer.gd
+++ addons/network-sync-rollback/debugger/PerfTimer.gd
@@ -15,10 +15,15 @@ func stop(name: String) -> void:
 	timings[name] = OS.get_ticks_usec() - start_timings[name]
 	start_timings.erase(name)
 
-func print_timings() -> void:
-	assert(start_timings.size() == 0, "there are unstopped timers: %s" % str(start_timings.keys()))
+func get_total() -> int:
 	var total := 0
 	for key in timings:
-		print ("%s: %s ms" % [key, float(timings[key]) / 1000.0])
 		total += timings[key]
+	return total
+
+func print_timings() -> void:
+	assert(start_timings.size() == 0, "there are unstopped timers: %s" % str(start_timings.keys()))
+	var total := get_total()
+	for key in timings:
+		print ("%s: %s ms" % [key, float(timings[key]) / 1000.0])
 	print(" * total: %s" % (float(total) / 1000.0))
