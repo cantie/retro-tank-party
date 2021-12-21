55a75828c13becf8ce130ffa2dc9fbdf7acf5e7d
David Snopek <dsnopek@gmail.com>
2021-12-21 16:42:12 -0600

    Attempt to make tracer target more stable (given reusing nodes and targets despawning)

--- mods/core/weapons/TracerBullet.gd
+++ mods/core/weapons/TracerBullet.gd
@@ -2,30 +2,32 @@ extends "res://src/objects/Bullet.gd"
 
 var target_seek_speed := 21845
 
-var target: Node2D = null
+var target_path: String = ''
 
 func _network_spawn_preprocess(info: Dictionary) -> Dictionary:
 	var res := ._network_spawn_preprocess(info)
-	res['target'] = info['target'].get_path() if info['target'] != null else null
+	res['target_path'] = str(info['target'].get_path()) if info['target'] != null else ''
 	return res
 
 func _network_spawn(info: Dictionary) -> void:
 	._network_spawn(info)
-	target = get_node(info['target']) if info['target'] != null else null
+	target_path = info['target_path']
 
 func _save_state() -> Dictionary:
 	var state = ._save_state()
-	state['target'] = target.get_path() if target else null
+	state['target_path'] = target_path
 	return state
 
 func _load_state(state: Dictionary) -> void:
 	._load_state(state)
-	target = get_node(state['target']) if state['target'] != null else null
+	target_path = state['target_path']
 
 func _network_process(delta: float, input: Dictionary) -> void:
 	._network_process(delta, input)
-	if target and is_instance_valid(target):
-		var target_vector = target.get_global_fixed_position().sub(get_global_fixed_position()).normalized()
-		vector = vector.linear_interpolate(target_vector, target_seek_speed).normalized()
-		fixed_rotation = vector.angle()
+	if target_path != '':
+		var target = get_node_or_null(target_path)
+		if target:
+			var target_vector = target.get_global_fixed_position().sub(get_global_fixed_position()).normalized()
+			vector = vector.linear_interpolate(target_vector, target_seek_speed).normalized()
+			fixed_rotation = vector.angle()
 
