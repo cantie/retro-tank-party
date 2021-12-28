9f5682e94e3b4e49c9936ae6cb7b74d508ae5c89
David Snopek <dsnopek@gmail.com>
2021-12-28 13:22:18 -0600

    Revert "Remove invalid arguments for strip on MacOS [build godot]"
    This reverts commit ef5ce74fa3effbcc42cf47607adb0bb2d210d33b.

--- scripts/godot/build-macosx.sh
+++ scripts/godot/build-macosx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OPTS="-j${NUM_CORES} platform=osx tools=no target=release production=yes osxcross_sdk=darwin20.2"
-STRIP="x86_64-apple-darwin20.2-strip"
+STRIP="x86_64-apple-darwin20.2-strip -u -r"
 
 case "$BUILD_TYPE" in
 	macosx-x86-64)
