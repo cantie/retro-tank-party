eeafb7625bcb5e6b71a39342450dd91253982550
David Snopek <dsnopek@gmail.com>
2022-08-19 12:02:14 -0500

    Got spectator camera controls mostly working

--- src/Game.gd
+++ src/Game.gd
@@ -73,7 +73,7 @@ func game_setup(_players: Dictionary, map_path: String, random_seed: int, _playe
 		var player = players[my_id]
 		_setup_player_camera(player_start_transforms[player.index - 1].get_origin().to_float())
 	else:
-		_setup_watch_camera()
+		_setup_player_camera(player_start_transforms[0].get_origin().to_float())
 
 	# Build up a list of possible contents for drawing randomly.
 	for pickup_path in Modding.find_resources("pickups"):
@@ -81,7 +81,24 @@ func game_setup(_players: Dictionary, map_path: String, random_seed: int, _playe
 		for i in range(pickup.rarity):
 			possible_pickups.append(pickup)
 
+	hud.clear_spectator_controls()
+	if SyncManager.spectating:
+		enable_watch_camera(true)
+
+		var watch_camera_button = Button.new()
+		watch_camera_button.text = 'All'
+		watch_camera_button.connect("pressed", self, "enable_watch_camera", [true])
+		hud.add_spectator_control('WatchCamera', watch_camera_button)
+
 	_game_setup()
+
+func _create_spectator_player_camera_button(player_id: int) -> void:
+	if players.has(player_id):
+		var player = players[player_id]
+		var player_camera_button = Button.new()
+		player_camera_button.text = player.name
+		player_camera_button.connect("pressed", self, "_on_spectator_player_camera_button_pressed", [player_id])
+		hud.add_spectator_control('Player_' + str(player_id), player_camera_button)
 
 func _game_setup() -> void:
 	hud.clear_all_labels()
@@ -122,11 +139,23 @@ func _on_SyncManager_scene_spawned(name: String, spawned_node: Node, scene: Pack
 
 		if peer_id == SyncManager.network_adaptor.get_network_unique_id():
 			spawned_node.player_controlled = true
-			_setup_player_camera(spawned_node.global_position)
-			spawned_node.camera = player_camera
-			_setup_player_listener(spawned_node)
+			_attach_player_camera(spawned_node)
+
+		if SyncManager.spectating:
+			_create_spectator_player_camera_button(peer_id)
 
 		emit_signal("player_spawned", spawned_node)
+
+func _clear_player_camera() -> void:
+	for child in players_node.get_children():
+		if "camera" in child:
+			child.camera = null
+
+func _attach_player_camera(player_node) -> void:
+	_clear_player_camera()
+	_setup_player_camera(player_node.global_position)
+	player_node.camera = player_camera
+	_setup_player_listener(player_node)
 
 func get_tank(player_id: int):
 	return players_node.get_node(str(player_id))
@@ -244,6 +273,7 @@ func remove_player(player_id) -> void:
 	kill_player(player_id)
 
 func enable_watch_camera(enable: bool = true) -> void:
+	_clear_player_camera()
 	player_camera.current = not enable
 	watch_camera.current = enable
 
@@ -253,12 +283,22 @@ func _on_player_dead(killer_id, tank) -> void:
 	if players_alive.has(peer_id):
 		players_alive.erase(peer_id)
 
-		var player_node = players_node.get_node(str(peer_id))
-		if player_node and player_node.player_controlled:
+		if tank.player_controlled:
 			_teardown_player_listener()
 			hud.clear_all_labels()
 
+		if SyncManager.spectating:
+			if tank.camera == player_camera:
+				_teardown_player_listener()
+				enable_watch_camera(true)
+			hud.remove_spectator_control('Player_' + str(peer_id))
+
 		emit_signal("player_dead", peer_id, killer_id)
+
+func _on_spectator_player_camera_button_pressed(player_id: int) -> void:
+	var player_node = get_tank(player_id)
+	if player_node:
+		_attach_player_camera(player_node)
 
 # From https://stackoverflow.com/a/12996028/364763
 #
