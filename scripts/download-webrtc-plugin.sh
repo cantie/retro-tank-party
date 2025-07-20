ea4a181a317a5fc805f287e9cf7a0fec4a1e5db2
David Snopek <dsnopek@gmail.com>
2025-07-20 08:46:44 -0500

    Update webrtc-native for MacOS arm64 support

--- scripts/download-webrtc-plugin.sh
+++ scripts/download-webrtc-plugin.sh
@@ -3,6 +3,8 @@
 PLATFORM=${1:-all}
 BUILD=${2:-both}
 
+RELEASE_URL=https://github.com/godotengine/webrtc-native/releases/download/1.1.0-stable/godot-gdnative-webrtc.zip
+
 download() {
 	if `which wget >/dev/null 2>&1`; then
 		wget "$1" -O "$2"
@@ -11,18 +13,9 @@ download() {
 	fi
 }
 
-# Since all platforms are now in the same download, we aren't using $PLATFORM
-# for anything.
+# Since all platforms builds are now in the same download, we aren't using either
+# $PLATFORM or $BUILD for anything.
 
-if [ "$BUILD" = "release" -o "$BUILD" = "both" ]; then
-	download https://github.com/godotengine/webrtc-native/releases/download/0.5/godot-webrtc-native-release-0.5.zip godot-webrtc-native-release.zip
-	unzip -o -a godot-webrtc-native-release.zip -x '*.tres'
-	rm godot-webrtc-native-release.zip
-fi
-
-if [ "$BUILD" = "debug" -o "$BUILD" = "both" ]; then
-	download https://github.com/godotengine/webrtc-native/releases/download/0.5/godot-webrtc-native-debug-0.5.zip godot-webrtc-native-debug.zip
-	unzip -o -a godot-webrtc-native-debug.zip -x '*.tres'
-	rm godot-webrtc-native-debug.zip
-fi
-
+download "$RELEASE_URL" godot-webrtc-native.zip
+unzip -o -a godot-webrtc-native.zip
+rm godot-webrtc-native.zip
